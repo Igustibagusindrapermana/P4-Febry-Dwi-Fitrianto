5775711406596954c9fb9c803ddb24e8fa5c91fd
Igustibagusindrapermana <64622423+Igustibagusindrapermana@users.noreply.github.com>
2020-05-12 20:45:22 +0700

    Create Jobsheet 10 C.cpp
    Array 3 dimensi

---
+++ Jobsheet 10 C.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+	printf(" Nama : I Gusti Bagus Indra Permana\n");
+	printf(" NIM : F1B019066\n");
+	printf(" Kelompok : 15 \n");
+
+int NIM [1][3][4];
+int i, j,k;
+for (i = 0; i< 1; i++){
+for (j = 0; j <3; j++){
+for (k= 0 ; k <4 ; k++)
+{
+printf ("NIM [%d][%d][%d] = ",i,j,k) ;
+scanf("%i%j%k",&NIM[i][j][k]) ;
+	 }
+	 printf ("\n") ;
+}
+}
+}
