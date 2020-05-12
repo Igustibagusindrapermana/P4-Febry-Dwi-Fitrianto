451f011a49e07831fc21eb6a9d80b2462cb67a56
Igustibagusindrapermana <64622423+Igustibagusindrapermana@users.noreply.github.com>
2020-05-12 20:35:13 +0700

    Create Jobsheet 8 C.cpp
    Mengisi dan menampilkan elemen array menggunakan perulangan

---
+++ Jobsheet 8 C.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+	printf(" Nama : I Gusti Bagus Indra Permana\n");
+	printf(" NIM : F1B019066\n");
+	printf(" Kelompok : 15 \n");
+	int i ;
+	int matriks[5] ;
+	 for (i=1 ; i<=5 ;i++){
+	 	printf ("Masukkan Nilai ke- %d = ",i) ;
+	 	scanf("%i",&matriks[i]) ;
+	 }
+	 printf("\n") ;
+	 for (i=1 ; i<=6 ;i++){
+	 	printf ("Nilai ke- %i = %d \n ",i,matriks[i]) ;
+}
+printf ("\n") ; }
