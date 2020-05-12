3879dcdddec996c4744c84aafb5ea14def3842ce
Igustibagusindrapermana <64622423+Igustibagusindrapermana@users.noreply.github.com>
2020-05-12 20:30:40 +0700

    Create Jobsheet 7 C.cpp
    Mengisi Elemen Array menggunakan perulangan

---
+++ Jobsheet 7 C.cpp
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+int main (){
+	printf(" Nama : I Gusti Bagus Indra Permana\n");
+	printf(" NIM : F1B019066\n");
+	printf(" Kelompok : 15 \n");
+	
+	int i ;
+	int matriks[5] ;
+	 for (i=1 ; i<=5 ;i++){
+	 	printf ("Masukkan Nilai ke- %d = ",i) ;
+	 	scanf("%i",&matriks[i]) ;
+	 }
+	 printf("\n") ;
+
+}
