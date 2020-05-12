58580ecc17a9ced962c195c0573b44e4c0009c79
Igustibagusindrapermana <64622423+Igustibagusindrapermana@users.noreply.github.com>
2020-05-12 18:51:44 +0700

    Create Jobsheet 1 C .cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C .cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main (){
+	printf(" Nama : I Gusti Bagus Indra Permana\n");
+	printf(" NIM : F1B019066\n");
+	printf(" Kelompok : 15 \n");
+	int nim[6]= {16,4,1,19,66,10};
+	
+	printf ("Data ke-1 = %i\n",nim[0]);
+	printf ("Data ke-2 = %i\n",nim[1]);
+	printf ("Data ke-3 = %i\n",nim[2]);
+	printf ("Data ke-4 = %i\n",nim[3]);
+	printf ("Data ke-5 = %i\n",nim[4]);
+	printf ("Data ke-6 = %i\n",nim[5]);
+}
