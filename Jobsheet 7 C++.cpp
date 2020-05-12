e63fc3c475f247389aa0db4802920b1fff358df0
Igustibagusindrapermana <64622423+Igustibagusindrapermana@users.noreply.github.com>
2020-05-12 20:29:11 +0700

    Create Jobsheet 7 C++.cpp
     Mengisi Elemen Array menggunakan perulangan

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ; 
+
+int main (){
+	cout<<" Nama : I Gusti Bagus Indra Permana\n";
+	cout<<" NIM : F1B019066\n";
+	cout<<" Kelompok : 15 \n";
+	int i ;
+	int matriks[5] ;
+	 for (i=1 ; i<=5 ;i++){
+	 	cout << "Nilai ke-" << i<< " = " ;
+	 	cin >> matriks[i] ;
+	 }
+	 cout <<endl ; 
+}
