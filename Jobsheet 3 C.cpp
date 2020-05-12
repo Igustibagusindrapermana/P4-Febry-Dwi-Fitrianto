20f3df7faf9ac890a42583a53fda8280f0f817b1
Igustibagusindrapermana <64622423+Igustibagusindrapermana@users.noreply.github.com>
2020-05-12 19:09:32 +0700

    Create Jobsheet 3 C.cpp
    Penjumlahan matrix 2 dimensi

---
+++ Jobsheet 3 C.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+using namespace std;
+main(){
+	printf(" Nama : I Gusti Bagus Indra Permana\n");
+	printf(" NIM : F1B019066\n");
+	printf(" Kelompok : 15 \n");
+	
+	int matriks1 [2][2], matriks2 [2][2], i, j;
+	printf ("Matriks 1\n");
+	for (i = 0; i <= 1; i++){
+		for (j = 0; j <= 1; j++){
+			printf ("Elemen [%d][%d] : ", i, j);
+			scanf ("%d", &matriks1 [i][j]);
+		}
+	}
+	
+	printf ("\nMatriks 2\n");
+	for (i = 0; i <= 1; i++){
+
+		for (j = 0; j <= 1; j++){
+		printf ("Elemen [%d][%d] : ", i, j);
+		scanf ("%d", &matriks2 [i][j]);
+		}
+	}
+	
+	printf ("\nHasil pemjumlahan matriks1 + matriks2 adalah \n");
+	for (i = 0; i <= 1; i++){
+		for (j = 0; j <= 1; j++)
+		printf ("%d\t", matriks1 [i][j] + matriks2 [i][j]);
+		printf ("\n");
+	}
+}
+
