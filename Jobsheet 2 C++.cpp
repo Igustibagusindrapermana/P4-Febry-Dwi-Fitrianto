d759a9fc772698eca0745e0907167166804202f6
Igustibagusindrapermana <64622423+Igustibagusindrapermana@users.noreply.github.com>
2020-05-12 18:54:09 +0700

    Create Jobsheet 2 C++.cpp
    Mengisi dan Menampilkan Elemen Aray(Dinamis)

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+main (){
+	int nim[6];
+	cout<<" Nama : I Gusti Bagus Indra Permana\n";
+	cout<<" NIM : F1B019066\n";
+	cout<<" Kelompok : 15 \n";
+cout << "Masukkan nilai ke-1 ";
+cin >> nim[0];
+cout << "Masukkan nilai ke-2 ";
+cin >> nim[1];
+cout << "Masukkan nilai ke-3 ";
+cin >> nim[2];
+cout << "Masukkan nilai ke-4 ";
+cin >> nim[3];
+cout << "Masukkan nilai ke-5 ";
+cin >> nim[4];
+cout << "Masukkan nilai ke-6 ";
+cin >> nim[5];
+}
